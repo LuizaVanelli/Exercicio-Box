113b405c5f0b7239738dd2ba7b8080118ab5cea9
LuizaVanelli <luizavanelli.vanelli@gmail.com>
2021-08-28 23:39:51 -0300

    Atividade AsciiArt

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Exercicio_Box
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("+---------------+");
+            Console.Write("|");
+            Console.ForegroundColor = ConsoleColor.DarkCyan;
+            Console.Write(" Luiza Vanelli ");
+            Console.ForegroundColor = ConsoleColor.White; 
+            Console.WriteLine("|");
+            Console.WriteLine("+---------------+");
+        }
+    }
+}
